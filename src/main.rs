662f1c42273384dffaf3ef7fb1e6e0f784c0d033
wi11-holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2025-04-09 23:46:48 +1000

    * install clap * define relevant task structures * define all commands and options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use chrono::{DateTime, Local};
+use clap::{Parser, Subcommand};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Task {
+    id: u8,
+    description: String,
+    status: TaskStatus,
+    created_at: DateTime<Local>,
+    updated_at: DateTime<Local>,
+}
+
+#[derive(Debug, Deserialize, Serialize, Subcommand)]
+enum TaskStatus {
+    /// Show unstarted tasks
+    Todo,
+    /// Show in progress tasks
+    InProgress,
+    /// Show compelted tasks
+    Done,
+}
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Add a new task
+    Add { description: String },
+    /// Udpate a task's description
+    Update { id: u8, description: String },
+    /// Delete a task
+    Delete { id: u8 },
+    /// Mark a task as in progress
+    MarkInProgress { id: u8 },
+    /// Mark a task as done
+    MarkDone { id: u8 },
+    /// List all tasks
+    List {
+        #[command(subcommand)]
+        status: Option<TaskStatus>,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _cli = Cli::parse();
 }
